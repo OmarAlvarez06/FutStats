44fc584d7de8808a25db7b8f87a9264c63808a70
BrianHumbertoGaytanLomeli <brian.gaytan4368@alumnos.udg.mx>
2021-06-21 09:36:01 -0500

    Cambiada Vista De Inicio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,47 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+
+    <div class="w-full relative mt-4 shadow-2xl rounded my-24 overflow-hidden">
+    <div class="top h-64 w-full bg-blue-600 overflow-hidden relative" >
+        <img src="https://images.unsplash.com/photo-1503264116251-35a269479413?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="" class="bg w-full h-full object-cover object-center absolute z-0">
+        <div class="flex flex-col justify-center items-center relative h-full bg-black bg-opacity-50 text-white">
+        <img src="{{Auth::user()->profile_photo_url}}" class="h-24 w-24 object-cover rounded-full">
+        <h1 class="text-2xl font-semibold">{{Auth::user()->name}}</h1>
+        <h4 class="text-sm font-semibold"><b>Miembro Desde </b>{{Auth::user()->created_at}}</h4>
+        </div>
+    </div>
+    <div class=" bg-white ">
+  
+        <div class="col-span-12 md:border-solid md:border-l md:border-black md:border-opacity-25 h-full pb-12 md:col-span-10">
+            <div class="px-4 pt-4">
+                <form action="#" class="flex flex-col space-y-8">
+        
+                <div>
+                    <h3 class="text-2xl font-semibold">Información Del Usuario</h3>
+                    <hr>
+                </div>
+        
+                <div class="form-item">
+                    <label class="text-xl ">Nombre</label>
+                    <input type="text" value="{{Auth::user()->name}}" class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200" disabled>
+                </div>
+        
+                <div class="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4">
+        
+                    <div class="form-item w-full">
+                    <label class="text-xl ">Rol</label>
+                    <input type="text" value="{{Auth::user()->rol}}" class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 " disabled>
+                    </div>
+        
+                    <div class="form-item w-full">
+                    <label class="text-xl ">Correo</label>
+                    <input type="text" value="{{Auth::user()->email}}" class="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 " disabled>
+                    </div>
+                </div>
+        
+        
+                </form>
             </div>
         </div>
+   
     </div>
 </x-app-layout>
